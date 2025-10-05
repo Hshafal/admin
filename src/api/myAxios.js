1c80a76fdeb5f3a94e26428b2728ec63a3a4dae7
eskander-taher <eskander.alshaibany@gmail.com>
2025-10-05 13:01:30 +0300

    remove local server url

--- src/api/myAxios.js
+++ src/api/myAxios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.DEV ? "http://localhost:5000/api" : "https://f3cea4d78b5e.vps.myjino.ru/api";
+// export const BASE_URL = import.meta.env.DEV ? "http://localhost:5000/api" : "https://f3cea4d78b5e.vps.myjino.ru/api";
+export const BASE_URL =  "https://f3cea4d78b5e.vps.myjino.ru/api";
 
-export const UPLOAD_URL = import.meta.env.DEV
-	? "http://localhost:5000/uploads"
-	: "https://f3cea4d78b5e.vps.myjino.ru/uploads";
+export const UPLOAD_URL = "https://f3cea4d78b5e.vps.myjino.ru/uploads";
 
 
 
